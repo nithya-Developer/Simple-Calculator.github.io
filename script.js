56799eddd8e72fd711c8ebd2fea3d5d6d923ee72
nithya-Developer <nithi.nithyab@gmail.com>
2022-10-12 20:14:31 +0530

    updated css

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 let equal_pressed = 0;
 //Refer all buttons excluding AC and DEL
+
 let button_input = document.querySelectorAll(".input-button");
+
 //Refer input,equal,clear and erase
 let input = document.getElementById("input");
 let equal = document.getElementById("equal");
